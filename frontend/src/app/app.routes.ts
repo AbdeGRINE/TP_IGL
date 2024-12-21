bc766a34bfbbfb894d73722ef4e7b74d96d26c7e
Benzii <farestech6@gmail.com>
2024-12-22 00:42:08 +0100

    config routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { MedecinDashboardComponent } from './components/medecin-dashboard/medecin-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, //route to login by default.
+  { path: 'login', component: LoginComponent, title: 'Login Page' },
+  { path: 'medecin-dashboard', component: MedecinDashboardComponent, title:'Medecin Dashboard' },
+];
