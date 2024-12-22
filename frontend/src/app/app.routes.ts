bb48d7684723ff94091416ed89751393f346a8ce
Benzii <farestech6@gmail.com>
2024-12-22 16:55:13 +0100

    update medecin and admin dashboards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
-import { MedecinDashboardComponent } from './features/medecin/dashboard/medecin-dashboard.component';
+import { MedecinDashboardComponent } from './features/medecin/medecin-dashboard/medecin-dashboard.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { AdminDashboardComponent } from './features/admin/admin-dashboard/admin-dashboard.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, //route to login by default.
   { path: 'login', component: LoginComponent, title: 'Login Page' },
   { path: 'medecin-dashboard', component: MedecinDashboardComponent, title:'Medecin Dashboard' },
+  { path: 'admin-dashboard', component: AdminDashboardComponent, title:'Admin Dashboard' },
+
 ];
---
+++ frontend/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+
+@Component({
+  selector: 'app-admin-dashboard',
+  imports: [HeaderComponent],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.css'
+})
+export class AdminDashboardComponent {
+
+}
