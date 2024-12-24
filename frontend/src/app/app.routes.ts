9d8ed90112146ec3bdbfef035eba761033f43d26
amalmessaoud <ma_messaoud@esi.dz>
2024-12-24 12:33:04 +0100

    Added Patient Dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './features/auth/login/login.component';
 import { AdminDashboardComponent } from './features/admin/admin-dashboard/admin-dashboard.component';
 import { CreerDPIComponent } from './features/admin/creer-dpi/creer-dpi.component';
 import { AfficherDpiComponent } from './features/medecin/afficher-dpi/afficher-dpi.component';
+import { DashboardPatientComponent } from './features/patient/dashboard-patient/dashboard-patient.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, //route to login by default.
@@ -28,4 +29,9 @@ export const routes: Routes = [
     component: AfficherDpiComponent,
     title: 'Afficher DPI',
   },
+  {
+    path : 'dashboard-patient',
+    component: DashboardPatientComponent,
+    title: 'Patient Dashboard',
+  }
 ];
