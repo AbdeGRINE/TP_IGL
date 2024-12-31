4115f1132873b1bc6f3836748047c9c71f006e01
Khadidja <mk_foudili@esi.dz>
2024-12-31 20:21:19 +0100

    add status code + comment permission

--- bilan/urls.py
+++ bilan/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('saisir_resultat_bilan_radiologique/', views.saisir_resultat_bilan_radiologique),
     path('consulter_bilan_biologique/', views.consulter_bilan_biologique),
     path('consulter_bilan_radiologique/', views.consulter_bilan_radiologique),
+    path('envoyer_donnees_graphes/',views.envoyer_donnees_graphes)
 ]
--- bilan/views.py
+++ bilan/views.py
@@ -7,14 +7,18 @@ from .serializers import BilanSerializer, TestSerializer, GraphiqueSerializer, C
 from users.serializers import UserSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from users.models import Bilan, Test, CompteRendu, Image
+from users.models import Bilan, Test, CompteRendu, Image, Consultation
 from datetime import date
 from base64 import b64decode
+from rest_framework import status
 
 # Create your views here.
 
 @api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def demander_bilan(request: Request) -> Response:
+    # medcin
     serializer = BilanSerializer(data = request.data)
     if serializer.is_valid():
         default_status = StatusBilan.EN_COURS
@@ -22,29 +26,34 @@ def demander_bilan(request: Request) -> Response:
         default_graphique = StatusGraphique.NON_ATTACHE
         bilan = serializer.save(status= default_status, date_demande= default_date, graphique= default_graphique)
         bilan_data = BilanSerializer(instance = bilan).data
-        return Response({
-            "bilan":bilan_data
-        })
-    return Response(serializer.errors)
+        return Response(bilan_data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def consulter_bilans_biologiques_en_cours(request: Request) -> Response:
+    # laborantin
     bilans = Bilan.objects.filter(status=StatusBilan.EN_COURS, type=TypeBilan.BIOLOGIQUE)
     bilans_data = BilanSerializer(bilans, many=True).data
-    return Response(bilans_data)
+    return Response(bilans_data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def consulter_bilans_radiologiques_en_cours(request: Request) -> Response:
+    # radiologue
     bilans = Bilan.objects.filter(status=StatusBilan.EN_COURS, type=TypeBilan.RADIOLOGIQUE)
     bilans_data = BilanSerializer(bilans, many=True).data
-    return Response(bilans_data)
+    return Response(bilans_data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def saisir_resultat_bilan_biologique(request: Request) -> Response:
+    # laborantin
     graphique_data = {"graphique": request.data.get('graphique')}
     graphique_serializer = GraphiqueSerializer(data=graphique_data)
     tests_data = request.data.get('tests', [])
@@ -58,14 +67,15 @@ def saisir_resultat_bilan_biologique(request: Request) -> Response:
         bilan.status= StatusBilan.TERMINE
         bilan.redigant_laborantin= request.user.laborantin
         bilan.save()
-        return Response({"data": serializer.data})
-    return Response(serializer.errors,graphique_serializer.errors)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+    return Response(serializer.errors,graphique_serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def saisir_resultat_bilan_radiologique(request: Request) -> Response:
+    # radiologue
     compterendu_data = request.data.get('compterendu')
     compterendu_serailizer = CompteRenduSerializer(data= compterendu_data)
     image_data = request.data.get('image')
@@ -80,24 +90,26 @@ def saisir_resultat_bilan_radiologique(request: Request) -> Response:
         bilan.status= StatusBilan.TERMINE
         bilan.redigant_radiologue= request.user.radiologue
         bilan.save()
-        return Response({"compte_rendu_data":compterendu_serailizer.data,"image_data":image_serializer.data})
-    return Response(compterendu_serailizer.errors,image_serializer.errors)
+        return Response({"compte_rendu_data":compterendu_serailizer.data,"image_data":image_serializer.data}, status=status.HTTP_200_OK)
+    return Response(compterendu_serailizer.errors,image_serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def consulter_bilan_biologique(request: Request) -> Response:
+    # medcin patient infirmier
     bilan_id = request.data['bilan']
     bilan = Bilan.objects.get(id= bilan_id)
     bilan_data = BilanSerializer(bilan).data
     tests = Test.objects.filter(bilan= bilan)
     tests_data = TestSerializer(tests, many=True).data
-    return Response({"bilan": bilan_data,"tests": tests_data})
+    return Response({"bilan": bilan_data,"tests": tests_data}, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def consulter_bilan_radiologique(request: Request) -> Response:
+    # medcin patient infirmier
     bilan_id = request.data['bilan']
     bilan = Bilan.objects.get(id= bilan_id)
     bilan_data = BilanSerializer(bilan).data
@@ -105,4 +117,23 @@ def consulter_bilan_radiologique(request: Request) -> Response:
     compterendu_data = CompteRenduSerializer(compterendu).data
     image = Image.objects.get(compte_rendu = compterendu)
     image_data = ImageSerializer(image).data
-    return Response({"bilan": bilan_data,"compterendu": compterendu_data, "image": image_data})
+    return Response({"bilan": bilan_data,"compterendu": compterendu_data, "image": image_data}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def envoyer_donnees_graphes(request: Request) -> Response:
+    # medcin patient infirmier
+    dpi_id = request.data['dpi']
+    consultations = Consultation.objects.filter(dpi= dpi_id)
+    tests = []
+    if consultations:
+        for consultation in consultations:
+            bilans = Bilan.objects.filter(consultation= consultation, type= TypeBilan.BIOLOGIQUE, graphique= StatusGraphique.ATTACHE)
+            if bilans:
+                for bilan in bilans:
+                    tests += Test.objects.filter(bilan= bilan)
+    if tests:
+        tests_data = TestSerializer(tests, many=True).data 
+        return Response(tests_data, status=status.HTTP_200_OK)
+    return Response({"detail":"pas de graphiques attachés"}, status= status.HTTP_400_BAD_REQUEST)
