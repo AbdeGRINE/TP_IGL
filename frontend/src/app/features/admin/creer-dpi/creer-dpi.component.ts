bf58f70bd43900c75cd0b7b1f4882a94d70ff0a5
Benzii <farestech6@gmail.com>
2025-01-07 16:00:52 +0100

    creer-dpi is integrated

--- frontend/src/app/features/admin/creer-dpi/creer-dpi.component.ts
+++ frontend/src/app/features/admin/creer-dpi/creer-dpi.component.ts
@@ -6,22 +6,18 @@ import { ApiDataService } from '../../../services/api-data.service';
 import { AuthService } from '../../../services/auth.service';
 import { AuthResponse } from '../../../models/interfaces/interfaces';
 
-
-export interface Patient {
-  nom: string;
-  prenom: string;
-  date_naissance: string;
-  adresse: string;
-  //   n_tlph: string;
-  nss: string;
-  mutuelle: string;
+export interface dataPatient {
+  patient: {
+    nom: string;
+    prenom: string;
+    date_naissance: string;
+    adresse: string;
+    telephone: string;
+    nss: string;
+    mutuelle: string;
+    personne_a_contacter: string;
+  };
   medecin_traitant: string;
-  personne_a_contacter: PersonneAContacter;
-}
-
-export interface PersonneAContacter {
-  nom: string;
-  prenom: string;
 }
 
 @Component({
@@ -31,18 +27,18 @@ export interface PersonneAContacter {
   styleUrl: './creer-dpi.component.css',
 })
 export class CreerDPIComponent {
-  patient: Patient = {
+  patient: dataPatient = {
+    "patient": {
     nom: '',
     prenom: '',
     date_naissance: '',
+    telephone: '',
     adresse: '',
     nss: '',
     mutuelle: '',
-    medecin_traitant: '',
-    personne_a_contacter: {
-      nom: '',
-      prenom: '#',
+    personne_a_contacter: '',
     },
+    medecin_traitant: '',
   };
   authResponse: AuthResponse;
   constructor(
@@ -59,47 +55,48 @@ export class CreerDPIComponent {
 
   inputValidation(): boolean {
     if (
-      !this.patient.nom ||
-      !this.patient.prenom ||
-      !this.patient.nss ||
-      !this.patient.adresse ||
-      !this.patient.date_naissance ||
-      !this.patient.mutuelle ||
+      !this.patient.patient.nom ||
+      !this.patient.patient.prenom ||
+      !this.patient.patient.nss ||
+      !this.patient.patient.telephone ||
+      !this.patient.patient.adresse ||
+      !this.patient.patient.date_naissance ||
+      !this.patient.patient.mutuelle ||
       !this.patient.medecin_traitant ||
-      !this.patient.personne_a_contacter.nom
+      !this.patient.patient.personne_a_contacter
     ) {
       alert('Veuillez remplir tous les champs obligatoires!');
       return false;
     }
     //`/^[a-zA-Z\s]+$/ is a regular expression.
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.nom)) {
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.patient.nom)) {
       alert('Nom ne doit pas contenir de chiffres.');
       return false;
     }
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.prenom)) {
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.patient.prenom)) {
       alert('Prenom ne doit pas contenir de chiffres.');
       return false;
     }
-    if (!/^\d+$/.test(this.patient.nss)) {
+    if (!/^\d+$/.test(this.patient.patient.nss)) {
       alert('NSS doit être un nombre.');
       return false;
     }
-    // if (
-    //   !/^\d+$/.test(this.patient.n_tlph) ||
-    //   !/^\d{10}$/.test(this.patient.n_tlph) ||
-    //   !this.patient.n_tlph.startsWith('0')
-    // ) {
-    //   alert(
-    //     'Numéro de téléphone doit être un nombre! Comporter 10 chiffres et commencer par 0.'
-    //   );
-    //   return false;
-    // }
+    if (
+      !/^\d+$/.test(this.patient.patient.telephone) ||
+      !/^\d{10}$/.test(this.patient.patient.telephone) ||
+      !this.patient.patient.telephone.startsWith('0')
+    ) {
+      alert(
+        'Numéro de téléphone doit être un nombre! Comporter 10 chiffres et commencer par 0.'
+      );
+      return false;
+    }
 
     if (!/^[a-zA-Z\s]+$/.test(this.patient.medecin_traitant)) {
       alert('Medecin traitant ne doit pas contenir de chiffres.');
       return false;
     }
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.personne_a_contacter.nom)) {
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.patient.personne_a_contacter)) {
       alert('Personne à contacter ne doit pas contenir de chiffres.');
       return false;
     }
@@ -109,14 +106,13 @@ export class CreerDPIComponent {
   handleSaveDPI() {
     if (this.inputValidation()) {
       //format the date of birth:
-      this.formatDate(this.patient.date_naissance);
-      console.log("PATIENT: ", this.patient); 
+      this.formatDate(this.patient.patient.date_naissance);
+      console.log('PATIENT HERO: ', this.patient);
       this.apiDataService
         .post('dpi/creer/', this.patient, `${this.authService.getToken()}`)
         .subscribe({
           next: (response) => {
             alert('Le DPI a été créé avec succès !');
-            console.log(response);
             this.navigationService.navigateTo('/admin-dashboard');
           },
           error: (err) => {
