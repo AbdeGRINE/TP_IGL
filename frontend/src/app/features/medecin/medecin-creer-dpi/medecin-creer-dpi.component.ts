13e6a799dff64e09c3b4830775e65c977faa4e44
Benzii <farestech6@gmail.com>
2025-01-07 17:35:30 +0100

    creer-dpi for medecin is linked

--- frontend/src/app/features/medecin/medecin-creer-dpi/medecin-creer-dpi.component.ts
+++ frontend/src/app/features/medecin/medecin-creer-dpi/medecin-creer-dpi.component.ts
@@ -3,42 +3,59 @@ import { HeaderComponent } from '../../shared/header/header.component';
 import { NavigationService } from '../../../services/navigation.service';
 import { FormsModule } from '@angular/forms';
 import { ApiDataService } from '../../../services/api-data.service';
-import { Router, RouterModule,NavigationEnd, ActivatedRoute } from '@angular/router';
-import { DPI } from '../../../models/interfaces/interfaces';
+import {
+  Router,
+  RouterModule,
+  ActivatedRoute,
+} from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { AuthResponse } from '../../../models/interfaces/interfaces';
+
+export interface dataPatient {
+  patient: {
+    nom: string;
+    prenom: string;
+    date_naissance: string;
+    adresse: string;
+    telephone: string;
+    nss: string;
+    mutuelle: string;
+    personne_a_contacter: string;
+  };
+  medecin_traitant: string;
+}
 
 @Component({
   selector: 'app-medecin-creer-dpi',
   imports: [HeaderComponent, FormsModule, RouterModule],
   templateUrl: './medecin-creer-dpi.component.html',
-  styleUrl: './medecin-creer-dpi.component.css'
+  styleUrl: './medecin-creer-dpi.component.css',
 })
-export class MedecinCreerDpiComponent implements OnInit{
-  patient: DPI = {
-    id: '',
-    nom: '',
-    prenom: '',
-    NSS: '',
-    adresse: '',
-    telephone: '',
-    mutuelle: '',
-    medecin: '',
-    personneAContacter: '',
-    dateNaissance: new Date(),
-    dateDeCreation:  new Date(),
-    consultations: [],
-    soins: [],
-    decodeBase64 : '',
+export class MedecinCreerDpiComponent implements OnInit {
+  patient: dataPatient = {
+    patient: {
+      nom: '',
+      prenom: '',
+      date_naissance: '',
+      telephone: '',
+      adresse: '',
+      nss: '',
+      mutuelle: '',
+      personne_a_contacter: '',
+    },
+    medecin_traitant: '',
   };
-  
+  authResponse: AuthResponse;
   constructor(
+    private navigationService: NavigationService,
     private apiDataService: ApiDataService,
-    private route : Router,
-    private router: ActivatedRoute, 
-    
-  ) {}
-  ngOnInit(): void {
-
+    private authService: AuthService,
+    private route: Router,
+    private router: ActivatedRoute
+  ) {
+    this.authResponse = this.authService.getAuthResponse();
   }
+  ngOnInit(): void {}
 
   navigateToMedecinDashboard() {
     this.route.navigate(['/medecin-dashboard']);
@@ -46,68 +63,74 @@ export class MedecinCreerDpiComponent implements OnInit{
 
   inputValidation(): boolean {
     if (
-      !this.patient.nom ||
-      !this.patient.prenom ||
-      !this.patient.NSS ||
-      !this.patient.adresse ||
-      !this.patient.telephone ||
-      !this.patient.mutuelle ||
-      !this.patient.medecin ||
-      !this.patient.personneAContacter
+      !this.patient.patient.nom ||
+      !this.patient.patient.prenom ||
+      !this.patient.patient.nss ||
+      !this.patient.patient.telephone ||
+      !this.patient.patient.adresse ||
+      !this.patient.patient.date_naissance ||
+      !this.patient.patient.mutuelle ||
+      !this.patient.medecin_traitant ||
+      !this.patient.patient.personne_a_contacter
     ) {
       alert('Veuillez remplir tous les champs obligatoires!');
       return false;
     }
     //`/^[a-zA-Z\s]+$/ is a regular expression.
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.nom)) {
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.patient.nom)) {
       alert('Nom ne doit pas contenir de chiffres.');
       return false;
     }
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.prenom)) {
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.patient.prenom)) {
       alert('Prenom ne doit pas contenir de chiffres.');
       return false;
     }
-    if (!/^\d+$/.test(this.patient.NSS)) {
+    if (!/^\d+$/.test(this.patient.patient.nss)) {
       alert('NSS doit être un nombre.');
       return false;
     }
-
     if (
-      !/^\d+$/.test(this.patient.telephone) ||
-      !/^\d{10}$/.test(this.patient.telephone) ||
-      !this.patient.telephone.startsWith('0')
+      !/^\d+$/.test(this.patient.patient.telephone) ||
+      !/^\d{10}$/.test(this.patient.patient.telephone) ||
+      !this.patient.patient.telephone.startsWith('0')
     ) {
       alert(
         'Numéro de téléphone doit être un nombre! Comporter 10 chiffres et commencer par 0.'
       );
       return false;
     }
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.medecin)) {
+
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.medecin_traitant)) {
       alert('Medecin traitant ne doit pas contenir de chiffres.');
       return false;
     }
-    if (!/^[a-zA-Z\s]+$/.test(this.patient.personneAContacter)) {
+    if (!/^[a-zA-Z\s]+$/.test(this.patient.patient.personne_a_contacter)) {
       alert('Personne à contacter ne doit pas contenir de chiffres.');
       return false;
     }
     return true;
   }
-
   handleSaveDPI() {
-    // if (this.inputValidation()) {
-    //   this.apiDataService.post('DPIs', this.patient).subscribe({
-    //     next: (response) => {
-    //       alert('Patient saved successfully!');
-    //       console.log(response);
-    //       this.route.navigate(['/medecin-dashboard']);
-    //     },
-    //     error: (err) => {
-    //       alert('Failed to save patient. Please try again.');
-    //       console.error(err);
-    //     },
-    //   });
-    // }
-    this.navigateToMedecinDashboard();
+    if (this.inputValidation()) {
+      //format the date of birth:
+      this.formatDate(this.patient.patient.date_naissance);
+      this.apiDataService
+        .post('dpi/creer/', this.patient, `${this.authService.getToken()}`)
+        .subscribe({
+          next: (response) => {
+            alert('Le DPI a été créé avec succès !');
+            this.navigateToMedecinDashboard();
+          },
+          error: (err) => {
+            alert('Impossible d"enregistrer le DPI. Veuillez réessayer.');
+            console.error(err);
+          },
+        });
+    }
   }
 
+  formatDate(date: string): string {
+    const [year, month, day] = date.split('-');
+    return `${day}-${month}-${year}`;
+  }
 }
